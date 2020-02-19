46b94b47a069ad6320647ae681538111509145c3
Ivan Terehin <ITerex91@yandex.ru>
2020-02-19 18:23:05 +0300

    создание View, Шаблона домашней страницы, Базы, передача и параметров в шаблоны и рендеринг

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from .models import *
 
-# Create your views here.
+
+def index(request):
+    """
+    Функция отображения для домашней страницы сайта.
+    """
+    # Генерация "количеств" некоторых главных объектов
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    # Доступные книги (статус = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()  # Метод 'all()' применен по умолчанию.
+    # задание на дом урок 5
+    num_books_task = Book.objects.filter(title__contains='Гарри').count
+    num_genre_task = Genre.objects.filter(name__contains='Роман').count
+
+    # Отрисовка HTML-шаблона index.html с данными внутри
+    # переменной контекста context
+
+    return render(
+        request,
+        'index.html',
+        context={'num_books': num_books, 'num_instances': num_instances,
+                 'num_instances_available': num_instances_available, 'num_authors': num_authors,
+                 'num_books_task': num_books_task,
+                 'num_genre_task': num_genre_task}
+    )
